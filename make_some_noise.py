ac6f958c8d7e7ad5938739c84ad8284dd57f8b64
zohaibxrehman <zohaibxrehman@gmail.com>
2019-06-16 07:01:15 -0400

    Some good work

--- make_some_noise.py
+++ make_some_noise.py
@@ -20,17 +20,19 @@ import csv
 import numpy
 from helpers import play_sound, play_sounds, make_sine_wave_array
 
+# TODO MAKE ATTRIBUTES PRIVATE ALONG WITH DOCUMENTATION
+
 
 class SimpleWave:
     """
-
+    A sine wave.
 
     === Attributes ===
     freq: frequency of the simple wave
     duration: duration of the simple wave
     amp: amplitude of the simple wave
     """
-    freq: int # FIXME WHAT IS TYPE? INT OR FLOAT
+    freq: int
     duration: float
     amp: float
 
@@ -53,15 +55,17 @@ class SimpleWave:
         """Return true if this simple wave and other simple wave are not
         equal.
         """
-        return not self == other
+        return not (self.freq == other.freq and self.duration ==
+                    other.duration and self.amp == other.amp)
 
     def __add__(self,
                 other: ANYWAVE) -> ComplexWave:
-        """add"""
+        """Return a complex wave produced by the addition of this simple wave
+        and <other> simple wave."""
         return ComplexWave([self, other])
 
     def get_duration(self) -> float:
-        """Return duration of a SimpleWave instance in seconds."""
+        """Return duration of a simple wave instance in seconds."""
         return self.duration
 
     def play(self) -> numpy.ndarray:
@@ -73,33 +77,39 @@ class SimpleWave:
 
 class ComplexWave:
     """
-
+    A complex wave comprising of simple waves.
 
     === Attributes ===
     waves: list of simple waves that build this complex wave
     """
+    waves: typing.List[SimpleWave]
+
     def __init__(self, waves: typing.List[SimpleWave]) -> None:
-        """Initialises the complex wave"""
+        """Initialises the complex wave.
+        Precondition: duration of each simple wave should be
+        equal and len(waves) > 0.
+        """
         self.waves = waves
 
     def __add__(self,
                 other: ANYWAVE) -> ComplexWave:
-        """Return the sum of a complex wave and another wave."""
+        """Return the sum of a complex wave and another wave.
+        """
         if isinstance(other, SimpleWave):
             return ComplexWave(self.waves + [other])
-        elif isinstance(other, ComplexWave):
+        else:  # isinstance(other, ComplexWave)
             return ComplexWave(self.waves + other.waves)
 
     def complexity(self) -> int:
-        """Return the coplexity of the complex wave."""
+        """Return the complexity of the complex wave.
+        """
         return len(self.waves)
 
     def play(self) -> numpy.ndarray:
         """Return a numpy array which represents a wave based on the additive
         synthesis of its simple waves.
-
         """
-        if len(self.waves) != 0 and self.play_helper():
+        if len(self.waves) != 0 and self._play_helper():
             arr = self.waves[0].play()
             for wave in self.waves[1:]:
                 arr = arr + wave.play()
@@ -110,9 +120,10 @@ class ComplexWave:
                 return arr / abs_max
             else:
                 return arr
+        else:
+            return # FIXME
 
-
-    def play_helper(self) -> bool:
+    def _play_helper(self) -> bool:
         """
         Helper method for play(). Return true when all the waves have the
         same duration.
@@ -139,18 +150,19 @@ class ComplexWave:
         return max(lst)
 
     def simplify(self) -> None:
-        """ TODO: write a docstring for this method
-            REMEMBER: this is not a required part of the assignment
+        """REMEMBER: this is not a required part of the assignment
         """
-        pass  # TODO: finish this method body
+        pass
 
 
 class Note:
     """
+    A note is a series of different waves (complex or simple) played in order,
+    one after the other.
 
     === Attributes ===
     amplitude: amplitude of the Note
-    waves: list of simple/complex waves that build this note
+    waves: list of simple/complex waves that this note comprises of
     """
     amplitude: float
     waves: list
@@ -162,21 +174,12 @@ class Note:
         self.waves = waves
 
     def __add__(self, other: Note) -> Note:
-        """Return the sum of this note and the other note. This Note instance,
-        when played, should sound exactly the same as playing the first Note
-        instance followed by the second with no pause in between.
+        """Return the sum of this note and the other note.
         The amplitude of the result will be the greatest amplitude of the two
         combined Note instances.
         """
         beat = Note(self.waves + other.waves)
-        if isinstance(self, Rest) and isinstance(other, Rest):
-            beat.amplitude = 0
-        elif isinstance(self, Rest):
-            beat.amplitude = other.amplitude
-        elif isinstance(other, Rest):
-            beat.amplitude = self.amplitude
-        else:
-            beat.amplitude = max(self.amplitude, other.amplitude)
+        beat.amplitude = max(self.amplitude, other.amplitude)
         return beat
 
     def get_waves(self) -> typing.List[ANYWAVE]:
@@ -188,7 +191,7 @@ class Note:
         return self.waves
 
     def get_duration(self) -> float:
-        """Return the duration of the note."""
+        """Return the duration of this note."""
         dur = 0
         for wave in self.waves:
             dur = dur + wave.get_duration()
@@ -200,15 +203,18 @@ class Note:
             arr = self.waves[0].play()
             for wave in self.waves[1:]:
                 arr = numpy.append(arr, wave.play())
-                #  FIXME is append the way?
             return arr * self.amplitude
+        else:
+            return # FIXME
 
 
 class SawtoothWave(ComplexWave):
     """
+    A sawtooth wave is a complex wave composed of an infinite number of
+    components which follow a distinct pattern.
 
     === Attributes ===
-    waves: list of simple waves that build this complex wave
+    waves: list of simple waves that build this sawtooth wave
     """
     waves: list
 
@@ -222,38 +228,15 @@ class SawtoothWave(ComplexWave):
                                     amplitude / k))
         ComplexWave.__init__(self, waves)
 
-    # def __add__(self,
-    #             other: ANYWAVE) -> ComplexWave:
-    #     """ """
-
-    # def complexity(self) -> int:
-    #     """ """
-    #     return len()
-
-    # def play(self) -> numpy.ndarray:
-    #     """ """
-    #     pass
-
-    # def get_waves(self) -> typing.List[SimpleWave]:
-    #     """  """
-    #     pass
-
-    # def get_duration(self) -> float:
-    #     """ """
-    #     pass
-
-    # def simplify(self) -> None:
-    #     """
-    #         REMEMBER: this is not a required part of the assignment
-    #     """
-    #     pass
-
 
 class SquareWave(ComplexWave):
     """
+    A square wave is similar to a sawtooth wave but the kth component of a
+    square wave has a frequency equal to (2k-1)F and an amplitude
+    equal to A/(2k-1).
 
     === Attributes ===
-    waves: list of simple waves that build this complex wave
+    waves: list of simple waves that build this square wave
     """
     waves: list
 
@@ -267,76 +250,25 @@ class SquareWave(ComplexWave):
                                     amplitude / (2 * k - 1)))
         ComplexWave.__init__(self, waves)
 
-    # def __add__(self,
-    #             other: ANYWAVE) -> ComplexWave:
-    #     """ """
-    #     pass
-    #
-    # def complexity(self) -> int:
-    #     """ """
-    #     pass
-    #
-    # def play(self) -> numpy.ndarray:
-    #     """ """
-    #     pass
-    #
-    # def get_waves(self) -> typing.List[SimpleWave]:
-    #     """ """
-    #     pass
-    #
-    # def get_duration(self) -> float:
-    #     """ """
-    #     pass
-    #
-    # def simplify(self) -> None:
-    #     """
-    #         REMEMBER: this is not a required part of the assignment
-    #     """
-    #     pass
-
 
 class Rest(ComplexWave):
     """
+    A rest describing a period of silence where no sound is played.
 
+    === Attributes ===
+    waves: list of simple waves that build this rest
     """
-    waves: list
-    # amplitude: float
+    waves: typing.List[SimpleWave]
 
     def __init__(self, duration: float) -> None:
         """Initialise the rest"""
         ComplexWave.__init__(self, [SimpleWave(0, duration, 0)])
-        # self.amplitude = 1
-
-    # def __add__(self,
-    #             other: ANYWAVE) -> ComplexWave:
-    #     """ """
-    #     pass
-    #
-    # def complexity(self) -> int:
-    #     """ """
-    #     pass
-    #
-    # def play(self) -> numpy.ndarray:
-    #     """ """
-    #     pass
-    #
-    # def get_waves(self) -> typing.List[SimpleWave]:
-    #     """ """
-    #     pass  #
-    #
-    # def get_duration(self) -> float:
-    #     """ """
-    #     pass
-    #
-    # def simplify(self) -> None:
-    #     """
-    #         REMEMBER: this is not a required part of the assignment
-    #     """
-    #     pass
 
 
 class StutterNote(Note):
     """
+    A stutter note is a note that alternates between playing a sawtooth wave
+    for a fixed period of time followed by silence for the same period of time.
 
     === Attributes ===
     amplitude: amplitude of the Note
@@ -353,9 +285,9 @@ class StutterNote(Note):
         """
         waves = []
         for i in range(int(20 * duration)):
-            # FIXME int or ceil? int(floor) makes sense
             waves.append(SawtoothWave(frequency, 1/40, amplitude))
             waves.append(Rest(1 / 40))
+
         remainder = 20 * duration - int(20 * duration)
         number_of_loops = int(remainder / 0.025)
         super_remainder = remainder % 0.025
@@ -375,22 +307,6 @@ class StutterNote(Note):
         Note.__init__(self, waves)
         self.amplitude = amplitude
 
-    # def __add__(self, other: Note) -> Note:
-    #     """ """
-    #     pass  #
-    #
-    # def get_waves(self) -> typing.List[ANYWAVE]:
-    #     """ """
-    #     pass  #
-    #
-    # def get_duration(self) -> float:
-    #     """ """
-    #     pass  #
-    #
-    # def play(self) -> numpy.ndarray:
-    #     """ ""
-    #     pass  #
-
 
 class Instrument:
     """
@@ -402,9 +318,9 @@ class Instrument:
     note: Note
 
     def __init__(self) -> None:
-        """Initialises the baliset
+        """Initialises the instrument.
         """
-        self.note = Note([]) # FIXME init doesn't seem perfect
+        self.note = Note([])
 
     def get_duration(self) -> float:
         """Return the duration of the baliset.
@@ -428,6 +344,9 @@ class Instrument:
 
 class Baliset(Instrument):
     """
+    An instrument that has a fundamental frequency of 196 Hz and
+    plays a sawtooth wave
+
     === Attributes ===
     note: the currently stored note
     """
@@ -437,11 +356,6 @@ class Baliset(Instrument):
         """Initialises the baliset.
         """
         Instrument.__init__(self)
-
-    # def get_duration(self) -> float:
-    #     """Return the duration of the baliset.
-    #     """
-    #     return self.note.get_duration()
 
     def next_notes(self,
                    note_info: typing.List[typing.Tuple[str, float, float]]
@@ -455,23 +369,17 @@ class Baliset(Instrument):
             if info[0] != 'rest':
                 ratio_lst = info[0].strip().split(':')
                 ratio = float(ratio_lst[0]) / float(ratio_lst[1])
-                # FIXME optimize? int?
                 waves.append(SawtoothWave(int(196 * ratio), info[2], info[1]))
-                # FIXME BIGTIME: FLOAT VALUE FOR FREQ?
-                # FIXME !!!
-                # FIXME !!!
             else:
                 waves.append(Rest(info[2]))
         self.note = Note(waves)
 
-    # def play(self) -> numpy.ndarray:
-    #     """Return a numpy array which represents the baliset.
-    #     """
-    #     return self.note.play()
-
 
 class Holophonor(Instrument):
     """
+    An instrument that has a fundamental frequency of 65 Hz and
+    plays a stutter note.
+
     === Attributes ===
     note: the currently stored note
     """
@@ -480,11 +388,7 @@ class Holophonor(Instrument):
     def __init__(self) -> None:
         """Initialises the holophonor.
         """
-        Instrument.__init__(self) # comeback and FIXME. stutternote
-
-    # def get_duration(self) -> float:
-    #     """  """
-    #     pass  #
+        Instrument.__init__(self)
 
     def next_notes(self,
                    note_info: typing.List[typing.Tuple[str, float, float]]
@@ -498,32 +402,31 @@ class Holophonor(Instrument):
             ratio_lst = note_info[0][0].strip().split(':')
             if ratio_lst[0] == 'rest':
                 waves.append(Rest(note_info[0][2]))
-                print('initial rest')
+                # print('initial rest')
             else:
                 ratio = float(ratio_lst[0]) / float(ratio_lst[1])
-                waves.append(StutterNote(int(ratio * 65), note_info[0][2], note_info[0][1]))
+                waves.append(StutterNote(int(ratio * 65), note_info[0][2],
+                                         note_info[0][1]))
 
             for info in note_info[1:]:
                 ratio_lst = info[0].strip().split(':')
                 if ratio_lst[0] != 'rest':
                     ratio = float(ratio_lst[0]) / float(ratio_lst[1])
                     wave = StutterNote(int(ratio * 65), info[2], info[1])
-                    # FIXME optimize? int?
-                    print(waves, wave)
                     waves.append(wave)
-                    # FIXME BIGTIME: FLOAT VALUE FOR FREQ?
                 else:
                     wave = Rest(info[2])
                     waves.append(wave)
             self.note = Note(waves)
 
-    # def play(self) -> numpy.ndarray:
-    #     """  """
-    #     pass
-
 
 class Gaffophone(Instrument):
     """
+    An instrument that has a fundamental frequency of 131 Hz and
+    plays two square waves at the same time. The first wave’s frequency is
+    calculated normally but the second wave has a frequency that is 3/2 times
+    the first wave’s frequency.
+
     === Attributes ===
     note: the currently stored note
     """
@@ -533,10 +436,6 @@ class Gaffophone(Instrument):
         """Initialises the gaffophone.
         """
         Instrument.__init__(self)
-
-    # def get_duration(self) -> float:
-    #     """ """
-    #     pass
 
     def next_notes(self,
                    note_info: typing.List[typing.Tuple[str, float, float]]
@@ -550,26 +449,20 @@ class Gaffophone(Instrument):
             if info[0] != 'rest':
                 ratio_lst = info[0].strip().split(':')
                 ratio = float(ratio_lst[0]) / float(ratio_lst[1])
-                # FIXME optimize? int?
-                waves.append(SquareWave(int(131 * ratio), info[2], info[1]) +
-                             SquareWave(int(131 * 3/2 * ratio), info[2], info[1]))
-                # FIXME BIGTIME: FLOAT VALUE FOR FREQ?
-                # FIXME !!!
-                # FIXME !!!
+                waves.append(
+                    SquareWave(int(131 * ratio), info[2], info[1]) + SquareWave(
+                        int(131 * 3 / 2 * ratio), info[2], info[1]))
             else:
                 waves.append(Rest(info[2]))
         self.note = Note(waves)
-    # def play(self) -> numpy.ndarray:
-    #     """ """
-    #     pass
 
 
 def play_song(song_file: str, beat: float) -> None:
-    """ """
-    instrument_dict = play_song_helper_1(song_file)
-    print(instrument_dict)
-    argument_dict = play_song_helper_2(instrument_dict, beat)
-    print(argument_dict)
+    """
+    Plays the song file <song_file> played at a beat of <beat>.
+    """
+    instrument_dict = _play_song_helper_1(song_file)
+    argument_dict = _play_song_helper_2(instrument_dict, beat)
     seconds = max(len(argument_dict['Baliset']),
                   len(argument_dict['Holophonor']),
                   len(argument_dict['Gaffophone']))
@@ -583,7 +476,6 @@ def play_song(song_file: str, beat: float) -> None:
 
         if second < len(argument_dict['Holophonor']):
             h = Holophonor()
-            # print(argument_dict['Holophonor'][second])
             h.next_notes(argument_dict['Holophonor'][second])
             play_list.append(h)
 
@@ -595,8 +487,9 @@ def play_song(song_file: str, beat: float) -> None:
         play_sounds(play_list)
 
 
-def play_song_helper_1(song_file: str) -> dict:
-    """."""
+def _play_song_helper_1(song_file: str) -> dict:
+    """Helper function for play_song.
+    """
     instrument_dict = {}
     with open(song_file, 'r') as csvfile:
         reader = csv.reader(csvfile)
@@ -618,8 +511,10 @@ def play_song_helper_1(song_file: str) -> dict:
     return instrument_dict
 
 
-def play_song_helper_2(instruments: dict, beat: float) -> dict:
-    """"""
+def _play_song_helper_2(instruments: dict, beat: float) -> dict:
+    """
+    Another helper function for play_song.
+    """
     play_dict = {}
     for instrument in instruments:
         play_dict[instrument] = []
@@ -638,7 +533,7 @@ def play_song_helper_2(instruments: dict, beat: float) -> dict:
                     sample_duration = float(argument_list[3]) * beat
                     amp = float(argument_list[2])
                 remain += sample_duration
-                dur = sample_duration
+                dur = round(sample_duration, 2)
                 if remain > 1:
                     val = round(remain - 1, 2)
                     remain = 1
@@ -659,7 +554,8 @@ def play_song_helper_2(instruments: dict, beat: float) -> dict:
                     if remain != 0:
                         lst.append((phrase, amp, round(remain, 2)))
                     val = 0
-            if (instruments[instrument].index(note) == len(instruments[instrument]) - 1) and remain != 0:
+            if (instruments[instrument].index(note) == len(
+                    instruments[instrument]) - 1) and remain != 0:
                 lst.append(('rest', 1, round(1 - remain, 2)))
                 play_dict[instrument].append(lst)
     return play_dict
@@ -677,11 +573,11 @@ ANYWAVE = typing.TypeVar('ANYWAVE',
 
 if __name__ == '__main__':
     import python_ta
-    # python_ta.check_all(config={'extra-imports': ['helpers',
-    #                                               'typing',
-    #                                               'csv',
-    #                                               'numpy'],
-    #                             'disable': ['E9997']})
+    python_ta.check_all(config={'extra-imports': ['helpers',
+                                                  'typing',
+                                                  'csv',
+                                                  'numpy'],
+                                'disable': ['E9997']})
 
     # test step 4
     # waves = []
@@ -759,7 +655,7 @@ if __name__ == '__main__':
     #     play_sound(gaf)
     #     play_sound(hol)
     #     play_sound(bal2)
-    #     play_sound(hol)
+    #     play_sound(hol)-+9
     #     play_sound(gaf2)
     #     play_sound(hol)
 
@@ -767,11 +663,11 @@ if __name__ == '__main__':
     # a = play_song_helper_2(s, 0.5)
     # b = play_song_helper_2(s, 1)
 
-    s = play_song_helper_1('spanish_violin.csv')
-    a = play_song_helper_2(s, 0.2)
+    # s = play_song_helper_1('spanish_violin.csv')
+    # a = play_song_helper_2(s, 0.2)
     # b = play_song_helper_2(s, 0.5)
     # print(b['Baliset'][2], b['Holophonor'][2], b['Gaffophone'][2])
-    play_song('spanish_violin.csv', 0.2)
+    # play_song('spanish_violin.csv', 0.2)
     # play_song('swan_lake.csv', 0.2)
     # play_song('song.csv', 0.8)
     # count = 0
